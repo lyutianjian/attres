b25974b27323621c98106bcdf6d2793d968f8b5d
Lu Tianjian <163417332+lyutianjian@users.noreply.github.com>
2026-04-01 01:09:06 +0800

    Add files via upload

--- model.py
+++ model.py
@@ -112,7 +112,7 @@ class AttentionResiduals(nn.Module):
         self.queries = nn.Parameter(torch.zeros(2 * config.n_layer + 1, config.n_embd))
 
     def forward(self, history, query_idx, return_stats=False):
-        values = torch.stack(history, dim=0) # (depth, B, T, C)
+        values = torch.stack(history, dim=0) 
         keys = self.norm(values)
         logits = torch.einsum('c,nbtc->nbt', self.queries[query_idx], keys)
         weights = F.softmax(logits, dim=0)
@@ -181,7 +181,7 @@ class GPT(nn.Module):
         # "UserWarning: functional_call was passed multiple values for tied weights.
         # This behavior is deprecated and will be an error in future versions"
         # not 100% sure what this is, so far seems to be harmless. TODO investigate
-        self.transformer.wte.weight = self.lm_head.weight # https://paperswithcode.com/method/weight-tying
+        self.transformer.wte.weight = self.lm_head.weight
 
         # init all weights
         self.apply(self._init_weights)
